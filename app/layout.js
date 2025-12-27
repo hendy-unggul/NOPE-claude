f2f7508812f59723200ee03062ee8d8b32223a5e
NOPEfounderHD <nope.bukanguebanget@gmail.com>
2025-12-27 07:24:37 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,16 @@
+import './globals.css'
 
+export const metadata = {
+  title: 'NOPE - Bukan kamu yang aneh',
+  description: 'Bukan kamu yang aneh tapi systemnya yang broken!',
+  manifest: '/manifest.json',
+  themeColor: '#000000',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  )
+}
